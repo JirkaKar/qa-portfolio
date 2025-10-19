8361cc2240ac6ca71a5483c627b631ce533b2042
Jiří Karadžov <jiri.karadzov@gmail.com>
2025-10-19 18:26:00 +0200

    Fix ChromeDriver initialization for Selenium 4.24+

--- automation/web-ui/conftest.py
+++ automation/web-ui/conftest.py
@@ -1,12 +1,18 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture
 def driver():
-    opts = Options()
-    opts.add_argument("--headless=new")
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=opts)
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
+
     yield driver
     driver.quit()
